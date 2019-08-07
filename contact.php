7e6a62d6f0b0d0b0831cce3a7e9537a67caff3bd
Noel Kelly <noel.kelly@outlook.com.au>
2019-08-07 15:23:48 +1000

    Typo

--- contact.php
+++ contact.php
@@ -97,22 +97,22 @@
       <!-- Map Column -->
       <div class="col-lg-8 mb-4">
         <!-- Embedded Google Map -->
-        <iframe width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="httphttps://maps.google.com/maps?hl=en&amp;ie=UTF8&amp;ll=37.0625,-95.677068&amp;spn=56.506174,79.013672&amp;t=m&amp;z=4&amp;output=embed"></iframe>
+        <iframe width="100%" height="400px" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?hl=en&amp;ie=UTF8&amp;ll=37.0625,-95.677068&amp;spn=56.506174,79.013672&amp;t=m&amp;z=4&amp;output=embed"></iframe>
       </div>
       <!-- Contact Details Column -->
       <div class="col-lg-4 mb-4">
         <h3>Contact Details</h3>
         <p>
-          3481 Melrose Place
-          <br>Beverly Hills, CA 90210
-          <br>
+          Seven Hills
+          <br />Brisbane, QLD 4152
+          <br />
         </p>
         <p>
-          <abbr title="Phone">P</abbr>: (123) 456-7890
+          <abbr title="Phone">P</abbr>: +61 481 830 450
         </p>
         <p>
           <abbr title="Email">E</abbr>:
-          <a href="mailto:name@example.com">name@example.com
+          <a href="mailto:typeme@in.com">noel.kelly (at) outlook.com.au
           </a>
         </p>
         <p>
